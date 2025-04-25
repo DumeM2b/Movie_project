eb9021b760be632aceb0eb49bbccc7bb82bb3c62
DumeM2b <163656850+DumeM2b@users.noreply.github.com>
2025-04-25 10:25:36 +0200

    Update composer.tf

--- terraform/composer.tf
+++ terraform/composer.tf
@@ -3,21 +3,20 @@ resource "google_composer_environment" "movie_composer" {
   region = var.region
   
   config {
-    node_count = 3
     software_config {
-      image_version = "composer-3.0.0-airflow-2.5.1"  # Version de Composer 3
+      image_version = "composer-3-airflow-2.10.5"
+
+      pypi_packages = {
+          pandas = ""
+      }
+
     }
-    private_environment_config {
-      enable_private_endpoint = true
+
+    enable_private_builds_only = true
+
+    node_config {
       network = google_compute_network.movie_vpc.id
       subnetwork = google_compute_subnetwork.movie_subnet.id
     }
-
-    # Indiquer le bucket contenant les DAGs
-    dag_gcs_prefix = google_storage_bucket.dag_bucket.name
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
+}
 }
