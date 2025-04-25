5ebba3f1cf7226ae192d072da166b75e59c30b24
DumeM2b <163656850+DumeM2b@users.noreply.github.com>
2025-04-25 10:23:09 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,23 @@
 # provider.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.31.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 6.31.0"
+    }
+  }
+}
+
 provider "google" {
-  project     = var.project_id  
-  region      = var.region  
+  project     = var.project_id
+  region      = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
 }
