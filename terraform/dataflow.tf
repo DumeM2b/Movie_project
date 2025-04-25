2d2e829496e43c60fb26913541bfc54985f2d019
DumeM2b <163656850+DumeM2b@users.noreply.github.com>
2025-04-25 10:22:06 +0200

    Update dataflow.tf

--- terraform/dataflow.tf
+++ terraform/dataflow.tf
@@ -1,16 +1,19 @@
-resource "google_dataflow_job" "pubsub_to_bq" {
-  name   = "pubsub-to-bq-job"
-  region = var.region
-
-  template_gcs_path = "gs://dataflow-templates-us-central1/latest/flex/PubSub_to_BigQuery_Flex"
-
+# dataflow.tf
+resource "google_dataflow_flex_template_job" "pubsub_to_bq" {
+  provider = google-beta
+  name              = "pubsub-to-bq-job"
+  region            = var.region
+  container_spec_gcs_path = "gs://dataflow-templates-${var.region}/latest/flex/PubSub_to_BigQuery_Flex"
   parameters = {
-    inputTopic        = "projects/${var.project_id}/topics/log_movie"
-    outputTable       = "${var.project_id}:${var.bq_dataset_name}.log_movie"
-    createDisposition = "CREATE_IF_NEEDED"   
-    writeDisposition  = "WRITE_APPEND"       
+    inputTopic        = "projects/movieproject-457013/topics/movie-logs-topic"
+    outputTableSpec       = "${var.project_id}:${var.bq_dataset_name}.log_movie"
+    useStorageWriteApi = "false"
+    useStorageWriteApiAtLeastOnce = "false"
+    numStorageWriteApiStreams = "0"
+  
   }
 
-  on_delete = "cancel"
+  temp_location = "gs://${var.project_id}-temp-location/temp/"
   service_account_email = google_service_account.movie_service_account.email
+  skip_wait_on_job_termination = true
 }
